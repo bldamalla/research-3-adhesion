d4e2a185bb3cfb8e341c4d1e9a373534d847f6a3
Bon Leif Amalla <bonleifamalla@gmail.com>
2018-08-20 18:51:58 +0800

    Redefine Ensemble as a subtype of AbstractArray.
    Also define the needed functions not including setindex!.
    Do not mutate the ensemble and its contents.
    
    Add image and ensemble means

--- src/MaterialImage.jl
+++ src/MaterialImage.jl
@@ -1,5 +1,11 @@
 # type definition for Images and Ensembles
 
+import Base: getindex, firstindex, lastindex, eltype
+import Base: push!, length
+import Base: size
+
+import Statistics: mean
+
 struct MaterialImage
   data::Vector{T} where {T<:AbstractFloat}
   dims::NTuple{2, Int}
@@ -16,15 +22,41 @@ struct MaterialImage
   end
 end
 
-struct Ensemble
-  data::Dict{Int, MaterialImage}
-  sz::Int
-end
-
 function MaterialImage(data::Array{T, 2}, discs::Int) where {T<:AbstractFloat}
   return MaterialImage(vec(data), size(data), discs)
 end
+size(img::MaterialImage) = img.dims
+eltype(img::MaterialImage) = Dict{Symbol, Union{Int, NTuple{2, Int}}}(:dims=>size(img), :discs=>img.discs)
+function mean(img::MaterialImage)
+  return mean(img.data)
+end
 
-function Ensemble()
-  return Ensemble(Dict{Int, MaterialImage}, 0)
+struct Ensemble <: AbstractArray{MaterialImage, 1}
+  data::Dict{Int, MaterialImage}
+end
+
+firstindex(::Ensemble) = 1
+length(ens::Ensemble) = length(ens.data)
+lastindex(ens::Ensemble) = length(ens)
+getindex(ens::Ensemble, i::Int) = get(ens.data, i, throw(KeyError(i)))
+function eltype(ens::Ensemble)
+  length(ens) < 1 || throw(ErrorException("cannot get element type of empty Ensemble"))
+  return Dict{Symbol, Union{Int, NTuple{2, Int}}}(:dims=>size(ens[1]), :discs=>ens[1].discs)
+end
+function push!(ens::Ensemble, v::MaterialImage)
+  sz = length(ens) + 1
+  if sz != 1
+    eltype(ens) != eltype(v) && throw(ErrorException("image must have the same dims and discs"))
+  end
+  push!(ens.data, sz=>v)
+end
+function mean(ens::Ensemble)
+  length(ens) == 0 || throw(ErrorException("ensemble has no elements"))
+  sm = zeros(ens[1].data)
+  for i in 1:length(ens)
+    for j in 1:length(ens[1].data)
+      @inbounds sm[j] += ens[i][j] / length(ens)
+    end
+  end
+  return MaterialImage(sm, ens[1].dims, ens[1].discs)
 end
