674be42b89b065ad8a5d0aa6317d1c28b9b0dd2d
Bon Leif Amalla <bonleifamalla@gmail.com>
2018-08-23 11:09:02 +0800

    Put FFT functions to prepare for two-pt stats.

--- src/pt-stats.jl
+++ src/pt-stats.jl
@@ -1,6 +1,22 @@
 # n-point statistics
 include("./MaterialImage.jl")
 
+using FFTW
+
 function one_pt_stat(ens::Ensemble)
   return mean(mean(arr))
+end
+
+function plan_fft(ens::Ensemble)
+  gen = rand(length(ens[1].data))
+  return plan_fft(gen)
+end
+
+function fft(ens::Ensemble)
+  holder = Vector{Vector{Complex}}
+  plan = plan_fft(ens)
+  for i in 1:length(ens)
+    push!(holder, plan * ens[i].data)
+  end
+  return holder
 end
