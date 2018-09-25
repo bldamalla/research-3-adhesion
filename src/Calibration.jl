27c54e0278a2d4d17a5a1edb1e926b116c295380
Bon Leif Amalla <bonleifamalla@gmail.com>
2018-09-25 08:17:21 +0800

    Add calibration "functions"

---
+++ src/Calibration.jl
@@ -0,0 +1,10 @@
+# define calibration functions
+
+"""
+Definitions for circuit calibration
+"""
+
+# time dependent resistance (Resistance vs time)
+# fit IV curve (Ohm's empirical line) (Current vs Voltage)
+# fit IV curve (net average resistance) (Current vs Voltage)
+# calibrate timer positions
