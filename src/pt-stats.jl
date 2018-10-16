c050b88f1d5b64ee129d9b18e8010186297af1de
Bon Leif Amalla <bonleifamalla@gmail.com>
2018-10-16 11:42:31 +0800

    Give better documentation on pt stats

--- src/pt-stats.jl
+++ src/pt-stats.jl
@@ -3,15 +3,24 @@ include("./MaterialImage.jl")
 
 using FFTW
 
+"""
+Calculate one-pt statistic from Ensemble
+"""
 function one_pt_stat(ens::Ensemble)
   return mean(mean(arr))
 end
 
+"""
+Plan fft routine based on ensemble data
+"""
 function plan_fft(ens::Ensemble)
   gen = rand(length(ens[1].data))
   return plan_fft(gen)
 end
 
+"""
+Perform fft on ensemble
+"""
 function fft(ens::Ensemble)
   holder = Vector{Vector{Complex}}
   plan = plan_fft(ens)
@@ -21,6 +30,9 @@ function fft(ens::Ensemble)
   return holder
 end
 
+"""
+Get the two-pt statistics on Ensemble data
+"""
 function two_pt_stat(ens::Ensemble)
   mn = mean(fft(ens))
   return real(ifft(mn))
