0101f4b395ebf5439c796c2ea869c4b59d87984c
Bon Leif Amalla <bonleifamalla@gmail.com>
2018-10-18 09:07:46 +0800

    Properly compute point statistics
    Revise according to Ensemble definition
    Import necessary FFTW material
    Define according to needs
    Properly compute and shift two-pt-stat output

--- src/pt-stats.jl
+++ src/pt-stats.jl
@@ -1,19 +1,19 @@
 # n-point statistics
 include("./MaterialImage.jl")
 
-using FFTW
+import FFTW: plan_fft, fft, ifft
 
 """
 Calculate one-pt statistic from Ensemble
 """
-function one_pt_stat(ens::Ensemble)
-  return mean(mean(arr))
+function one_pt_stat(ens::Vector{MaterialImage})
+  return mean(mean(ens))
 end
 
 """
 Plan fft routine based on ensemble data
 """
-function plan_fft(ens::Ensemble)
+function plan_fft(ens::Vector{MaterialImage})
   gen = rand(length(ens[1].data))
   return plan_fft(gen)
 end
@@ -21,19 +21,27 @@ end
 """
 Perform fft on ensemble
 """
-function fft(ens::Ensemble)
-  holder = Vector{Vector{Complex}}
+function fft(ens::Vector{MaterialImage})
+  holder = Vector{Vector{Complex{Float64}}}()
   plan = plan_fft(ens)
   for i in 1:length(ens)
-    push!(holder, plan * ens[i].data)
+    push!(holder, plan * vec(ens[i].data))
   end
-  return holder
+  return mean(holder)
 end
 
 """
 Get the two-pt statistics on Ensemble data
 """
-function two_pt_stat(ens::Ensemble)
-  mn = mean(fft(ens))
-  return real(ifft(mn))
+function two_pt_stat(ens::Vector{MaterialImage})
+  ft_ens = fft(ens); cc = conj(ft_ens)
+  # convolution theorem produces asymmetric vector
+  off_center = real(ifft(ft_ens .* cc)) ./ length(ens[1].data)
+  ret = similar(off_center)
+  # assume odd length for return vector
+  shift = div(length(ret), 2)
+  for i in 1:length(ret)
+    ret[(shift+i) % length(ret) == 0 ? length(ret) : ((shift+i) % length(ret))] = off_center[i]
+  end
+  return ret
 end
