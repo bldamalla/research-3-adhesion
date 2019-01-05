9d55fd59fef24c8a36dea5787db52fd29490d2a6
Bon Leif Amalla <bonleifamalla@gmail.com>
2019-01-05 22:02:45 +0800

    Add status updates for running

--- scripts/pt-stats.jl
+++ scripts/pt-stats.jl
@@ -28,7 +28,9 @@ tk = @distributed for f in 1:64 ### START OF MAIN FOR LOOP
   @nexprs 3 i -> begin
     @nexprs 4 j -> begin
       one_pt_i_j = one_pt_stat(ens_i_j)
+      println("Calculated: 1-pt ($(f), $(i), $(j))")
       two_pt_i_j, two_pt_i_j_ave = two_pt_stat(ens_i_j)
+      println("Calculated: 2-pt ($(f), $(i), $(j))")
       push!(evone, one_pt_i_j)
     end
   end
@@ -36,11 +38,13 @@ tk = @distributed for f in 1:64 ### START OF MAIN FOR LOOP
   # compare the point statistics of the ensembles
   one_pt = bar(evone)
   savefig(one_pt, "./plots/one_pt/evone_$(f).png")
+  println("Plotted: 1-pt ($(f))")
 
   @nexprs 3 i -> begin
     @nexprs 4 j -> begin
       tpt_i_j = contour(-112:112, -112:112, reshape(two_pt_i_j_ave, 225, 225))
       savefig(tpt_i_j, "./plots/two_pts/two_pt_$(i)_$(j)_$(f).png")
+      println("Plotted: 2-pt ($(f), $(i), $(j))")
     end
   end
 
@@ -67,6 +71,7 @@ tk = @distributed for f in 1:64 ### START OF MAIN FOR LOOP
     vars[(i-1)*3+1, f] = tr(q'q)
 
     savefig("plots/PCA/mdl_$(f)_$(i).png")
+    println("Plotted: PCA ($(f), $(i))")
   end
 
 end ### END MAIN PLOT LOOP
